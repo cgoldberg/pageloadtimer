d6c1c55f2b44e3eacb6d0e57dce36e4f123cda9e
Corey Goldberg <cgoldberg@gmail.com>
2015-01-13 14:26:10 -0500

    added initial version of setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+
+setup(
+    name='navtimer',
+    version='0.1.0',
+    author='Corey Goldberg',
+    author_email='cgoldberg@gmail.com',
+    py_modules=['navtimer',],
+    url='https://github.com/cgoldberg/navtimer',
+    license='MIT',
+    description='Automated Metric Collection from Web Navigation Timing API',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'pyvirtualdisplay >= 0.1.5'
+        'easyprocess >= 0.1.6',
+        "selenium >= 2.44.0",
+    ],
+)

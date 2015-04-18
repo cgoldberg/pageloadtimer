4a1b1e05375ac389ce4290821ab75a5ed69437b4
Corey Goldberg <cgoldberg@gmail.com>
2015-04-18 14:20:44 -0400

    return OrderedDict

--- pageloadtimer.py
+++ pageloadtimer.py
@@ -4,14 +4,12 @@
 # License: MIT
 
 
+import collections
 import logging
 import textwrap
 
 from pyvirtualdisplay import Display
 from selenium import webdriver
-
-logging.basicConfig(level=logging.INFO, format='%(message)s',)
-logger = logging.getLogger(__name__)
 
 
 class PageLoadTimer:
@@ -19,14 +17,15 @@ class PageLoadTimer:
     def __init__(self, driver):
         self.driver = driver
 
-
-    def _execute_timing_js(self):
-        jscript = textwrap.dedent("""
+        self.jscript = textwrap.dedent("""
             var performance = window.performance || {};
             var timings = performance.timing || {};
             return timings;
             """)
-        timings = self.driver.execute_script(jscript)
+
+
+    def inject_timing_js(self):
+        timings = self.driver.execute_script(self.jscript)
         return timings
 
 
@@ -38,18 +37,19 @@ class PageLoadTimer:
                           'domContentLoadedEventStart', 'domContentLoadedEventEnd',
                           'domComplete', 'loadEventStart', 'loadEventEnd'
                           )
-        timings = self._execute_timing_js()
+        timings = self.inject_timing_js()
         min_time = min((epoch for epoch in timings.values() if epoch != 0))
         event_times = ([(event, timings[event] - min_time) for event
                        in ordered_events if event in timings])
-        return event_times
+        event_times_ordered = collections.OrderedDict(event_times)
+        return event_times_ordered
 
 
 
 if __name__ == '__main__':
-        url = 'http://www.google.com'
-        driver = webdriver.Firefox()
-        driver.get(url)
-        timer = PageLoadTimer(driver)
-        print timer.get_event_times()
-        driver.quit()
+    url = 'http://www.example.com'
+    driver = webdriver.Firefox()
+    driver.get(url)
+    timer = PageLoadTimer(driver)
+    print timer.get_event_times()
+    driver.quit()
